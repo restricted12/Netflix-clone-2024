1a8b4b5668d405294852cce1402580b1d0168399
Ezedin <ezepayooner@gmail.com>
2024-11-20 04:54:09 -0800

    banner

--- Netflix/src/pages/Home/Home.jsx
+++ Netflix/src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Footer from '../../components/footer/footer'
 import Header from '../../components/header/header'
+import Banner from '../../components/Banner/Banner.jsx'
 const Home = () => {
   return (
     <>
       <Header/>
+      <Banner/>
       <Footer/>
     </>
   )
