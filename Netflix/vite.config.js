bfb1a72c39e285106d83adce3594ffc242267b71
Ezedin <ezepayooner@gmail.com>
2024-11-21 05:30:15 -0800

    vite

--- Netflix/vite.config.js
+++ Netflix/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Netflix-clone-2024/', // Ensure this matches your repository name
+  base: '/<Netflix-clone-2024>/'
 });
