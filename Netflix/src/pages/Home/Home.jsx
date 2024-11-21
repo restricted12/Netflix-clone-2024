a99d442ac4da402145889e9136dff368a4d0a168
Ezedin <ezepayooner@gmail.com>
2024-11-21 03:40:11 -0800

    row

--- Netflix/src/pages/Home/Home.jsx
+++ Netflix/src/pages/Home/Home.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import Footer from '../../components/footer/footer'
 import Header from '../../components/header/header'
 import Banner from '../../components/Banner/Banner.jsx'
+import RowList from '../../components/Rows/RowList/RowList.jsx'
 const Home = () => {
   return (
     <>
       <Header/>
       <Banner/>
+      <RowList/>
       <Footer/>
     </>
   )
